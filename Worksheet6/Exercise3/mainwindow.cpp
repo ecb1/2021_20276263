594c30961f715fd8aa4d6875328b9295351e3484
Edwin Cockeye-Brown <eeyec3@nottingham.ac.uk>
2022-04-04 15:32:13 +0100

    Worksheet 6 Completed 11/03/2022

--- Worksheet6/Exercise3/mainwindow.cpp
+++ Worksheet6/Exercise3/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include <vtkSmartPointer.h>
 #include <vtkCubeSource.h>
+#include <vtkSphereSource.h>
 #include <vtkActor.h>
 #include <vtkProperty.h>
 #include <vtkCamera.h>
@@ -15,6 +16,12 @@
 #include <vtkSTLReader.h>
 #include <QFileDialog>
 #include <QMessageBox>
+#include <QStatusBar>
+#include <QFileDialog>
+#include <QInputDialog>
+#include <QDebug> 
+#include <QColorDialog>
+#include <QAbstractButton>
 
 
 #include "mainwindow.h"
@@ -24,12 +31,6 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
 {
     // standard call to setup Qt UI (same as previously)
     ui->setupUi( this );
-
-	//Set icon paths
-	ui->actionFileSave->setIcon(QIcon("C:\Users\ecb\Desktop\2021_20276263\Worksheet6\Exercise3\Icons\filesave.png"));
-	ui->actionFileOpen->setIcon(QIcon("C:\Users\ecb\Desktop\2021_20276263\Worksheet6\Exercise3\Icons\fileopen.png"));
-	ui->actionHelp->setIcon(QIcon("C:\Users\ecb\Desktop\2021_20276263\Worksheet6\Exercise3\Icons\help.png"));
-	ui->actionPrint->setIcon(QIcon("C:\Users\ecb\Desktop\2021_20276263\Worksheet6\Exercise3\Icons\print.png"));
 	
 	// Connect the released() signal of each action object to the action slot in each object
 	connect(this, &MainWindow::statusUpdateMessage, ui->statusbar, &QStatusBar::showMessage);
@@ -37,6 +38,16 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
 	connect(ui->actionFileOpen, &QAction::triggered, this, &MainWindow::handleOpenButton);
 	connect(ui->actionHelp, &QAction::triggered, this, &MainWindow::handleHelpButton);
 	connect(ui->actionPrint, &QAction::triggered, this, &MainWindow::handlePrintButton);
+	connect(ui->ModelColourButton, &QPushButton::released, this, &MainWindow::handleModelColorButton);
+	connect(ui->BackgroundColourButton, &QPushButton::released, this, &MainWindow::handleBackgroundColorButton);
+	connect(ui->shrinkFilter_checkBox, &QCheckBox::toggled, this, &MainWindow::handleShrinkFilterCheckBox );
+	connect(ui->clipFilter_checkBox, &QCheckBox::toggled, this, &MainWindow::handleClipFilterCheckBox );
+	connect(ui->actionCube, &QAction::triggered, this, &MainWindow::handleRenderCube);
+	connect(ui->actionSphere, &QAction::triggered, this, &MainWindow::handleRenderSphere);
+
+	ShrinkFilterStatus = ui->shrinkFilter_checkBox->isChecked();
+	ClipFilterStatus = ui->clipFilter_checkBox->isChecked();
+
 
 	//Create and Setup Renderer Window
 	//Now need to create a VTK render windowand link it to the QtVTK widget
@@ -48,53 +59,6 @@ MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWi
 	//vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();		// ###### We've already created the renderWindow this time ######
 	ui->qvtkWidget->GetRenderWindow()->AddRenderer(renderer);									// ###### ask the QtVTKOpenGLWidget for its renderWindow ######
 
-
-
-	/*  EXERCISE 2 CODE FOR REFERENCE
-	//Now need to create a VTK render windowand link it to the QtVTK widget
-	vtkNew<vtkGenericOpenGLRenderWindow> renderWindow;
-	ui->qvtkWidget->SetRenderWindow( renderWindow );			// note that vtkWidget is the name I gave to my QtVTKOpenGLWidget in Qt creator
-	
-	// Now use the VTK libraries to render something. To start with you can copy-paste the cube example code, there are comments to show where the code must be modified.
-	//--------------------------------------------- Code From Example 1 --------------------------------------------------------------------------
-	// Create a cube using a vtkCubeSource
-	vtkSmartPointer<vtkCubeSource> cubeSource = vtkSmartPointer<vtkCubeSource>::New();
-
-	// Create a mapper that will hold the cube's geometry in a format suitable for rendering
-	vtkSmartPointer<vtkDataSetMapper> mapper = vtkSmartPointer<vtkDataSetMapper>::New();
-	mapper->SetInputConnection( cubeSource->GetOutputPort() );
-
-	// Create an actor that is used to set the cube's properties for rendering and place it in the window
-	vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
-	actor->SetMapper(mapper);
-	actor->GetProperty()->EdgeVisibilityOn();
-
-	vtkSmartPointer<vtkNamedColors> colors = vtkSmartPointer<vtkNamedColors>::New();
-	actor->GetProperty()->SetColor( colors->GetColor3d("Red").GetData() );
-
-	// Create a renderer, and render window
-	vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
-	//vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();		// ###### We've already created the renderWindow this time ######
-	ui->qvtkWidget->GetRenderWindow()->AddRenderer( renderer );									// ###### ask the QtVTKOpenGLWidget for its renderWindow ######
-
-	// Link a renderWindowInteractor to the renderer (this allows you to capture mouse movements etc)  ###### Not needed with Qt ######
-	//vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = vtkSmartPointer<vtkRenderWindowInteractor>::New();
-	//renderWindowInteractor->SetRenderWindow( ui->vtkWidget->GetRenderWindow() );				
-
-	// Add the actor to the scene
-	renderer->AddActor(actor);
-	renderer->SetBackground( colors->GetColor3d("Silver").GetData() );
-
-	// Setup the renderers's camera
-	renderer->ResetCamera();
-	renderer->GetActiveCamera()->Azimuth(30);
-	renderer->GetActiveCamera()->Elevation(30);
-	renderer->ResetCameraClippingRange();
-
-	// Render and interact
-	//renderWindow->Render();					// ###### Not needed with Qt ######
-	//renderWindowInteractor->Start();			// ###### Not needed with Qt ######
-	--------------------------------------------- /Code From Example 1 -------------------------------------------------------------------------*/
 }
 
 MainWindow::~MainWindow()
@@ -110,15 +74,14 @@ void MainWindow::handleOpenButton() {
 	QByteArray ba = fileName.toLocal8Bit();
 	const char* c_fileName = ba.data();
 
-	vtkNew<vtkSTLReader> reader;
 	reader->SetFileName(c_fileName);
 	reader->Update();
 
+	source = reader;
+
 	//Code to display the STL file data
-	vtkNew<vtkPolyDataMapper> mapper;
 	mapper->SetInputConnection(reader->GetOutputPort());
 
-	vtkNew<vtkActor> actor;
 	actor->SetMapper(mapper);
 	actor->GetProperty()->EdgeVisibilityOn();
 
@@ -136,14 +99,13 @@ void MainWindow::handleOpenButton() {
 	renderer->ResetCameraClippingRange();
 
 
-	emit statusUpdateMessage(QString("CAD File has been opened"), 0);
+	emit statusUpdateMessage(QString("CAD (.stl) File has been opened"), 0);
 }
 
 
 void MainWindow::handleSaveButton() {
 	//Insert Code
 }
-
 
 void MainWindow::handleHelpButton() {
 	//Insert Code
@@ -152,4 +114,140 @@ void MainWindow::handleHelpButton() {
 
 void MainWindow::handlePrintButton() {
 	//Insert Code
+}
+
+void MainWindow::handleModelColorButton() {
+	//Open colour dialog to prompt user for colour of choice
+	QColor Color = QColorDialog::getColor(Qt::white, this, "Choose Model Colour");
+	
+	if (Color.isValid())
+	{
+		double red = Color.redF();
+		double green = Color.greenF();
+		double blue = Color.blueF();
+
+		actor->GetProperty()->SetColor(red, green, blue);
+	}
+
+	//Rerenders the model with the changed colour
+	renderWindow->Render();
+	ui->qvtkWidget->GetRenderWindow()->Render();
+
+	emit statusUpdateMessage(QString("Change Model Colour Button was clicked"), 0);
+}
+
+void MainWindow::handleBackgroundColorButton() {
+	//Open colour dialog to prompt user for colour of choice
+	QColor Color = QColorDialog::getColor(Qt::white, this, "Choose Background Colour");
+	
+	if (Color.isValid())
+	{
+		double red = Color.redF();
+		double green = Color.greenF();
+		double blue = Color.blueF();
+
+		renderer->SetBackground(red, green, blue);
+	}
+	
+	//Rerenders the background with the changed colour
+	renderWindow->Render();
+	ui->qvtkWidget->GetRenderWindow()->Render();
+
+	emit statusUpdateMessage(QString("Change Background Colour Button was clicked"), 0);
+}
+
+void MainWindow::handleShrinkFilterCheckBox(bool ShrinkFilterStatus) {
+	if (ShrinkFilterStatus == true) {
+		//ensure that no other filter is applied
+		ShrinkFilter->SetInputConnection(source->GetOutputPort());
+		ShrinkFilter->SetShrinkFactor(.5);
+		ShrinkFilter->Update();
+
+		mapper->SetInputConnection(ShrinkFilter->GetOutputPort());
+
+		//Display update message
+		emit statusUpdateMessage(QString("Shrink Filter Applied"), 0);
+	}
+
+	else {
+		mapper->SetInputConnection(source->GetOutputPort());
+	}
+
+	renderWindow->Render();
+	ui->qvtkWidget->GetRenderWindow()->Render();
+}
+
+
+
+void MainWindow::handleClipFilterCheckBox(bool ClipFilterStatus) {
+	if (ClipFilterStatus == true) {
+		planeLeft->SetOrigin(0.0, 0.0, 0.0);
+		planeLeft->SetNormal(-1.0, 0.0, 0.0);
+
+		ClipFilter->SetInputConnection(source->GetOutputPort());
+		ClipFilter->SetClipFunction(planeLeft.Get());
+		mapper->SetInputConnection(ClipFilter->GetOutputPort());
+
+		//Display Update Message
+		emit statusUpdateMessage(QString("Clip filter applied"), 0);
+	}
+	else
+	{
+		mapper->SetInputConnection(source->GetOutputPort());
+	}
+	
+	renderWindow->Render();
+	ui->qvtkWidget->GetRenderWindow()->Render();
+}
+
+void MainWindow::handleRenderCube() {
+	// Create a cube using a vtkCubeSource
+	vtkSmartPointer<vtkCubeSource> cubeSource = vtkSmartPointer<vtkCubeSource>::New();
+
+	source = cubeSource;
+
+	//mapper that will hold the cube's geometry in a format suitable for rendering
+	mapper->SetInputConnection(cubeSource->GetOutputPort());
+
+	//actor that is used to set the cube's properties for rendering and place it in the window
+	actor->SetMapper(mapper);
+	actor->GetProperty()->EdgeVisibilityOn();
+
+	actor->GetProperty()->SetColor(colors->GetColor3d("Red").GetData());
+
+	// Add the actor to the scene
+	renderer->AddActor(actor);
+	renderer->SetBackground(colors->GetColor3d("Silver").GetData());
+
+	// Setup the renderers's camera
+	renderer->ResetCamera();
+	renderer->GetActiveCamera()->Azimuth(30);
+	renderer->GetActiveCamera()->Elevation(30);
+	renderer->ResetCameraClippingRange();
+}
+
+void MainWindow::handleRenderSphere() {
+	// Create a sphere using a vtkCubeSource
+	vtkSmartPointer<vtkSphereSource> sphereSource = vtkSmartPointer<vtkSphereSource>::New();
+
+	source = sphereSource;
+
+	//mapper that will hold the cube's geometry in a format suitable for rendering
+	mapper->SetInputConnection(sphereSource->GetOutputPort());
+
+	//actor that is used to set the cube's properties for rendering and place it in the window
+	actor->SetMapper(mapper);
+	actor->GetProperty()->EdgeVisibilityOn();
+
+	actor->GetProperty()->SetColor(colors->GetColor3d("Red").GetData());
+
+	// Add the actor to the scene
+	renderer->AddActor(actor);
+	renderer->SetBackground(colors->GetColor3d("Silver").GetData());
+
+	// Setup the renderers's camera
+	renderer->ResetCamera();
+	renderer->GetActiveCamera()->Azimuth(30);
+	renderer->GetActiveCamera()->Elevation(30);
+	renderer->ResetCameraClippingRange();
 }
--- Worksheet6/Exercise3/mainwindow.h
+++ Worksheet6/Exercise3/mainwindow.h
@@ -4,6 +4,33 @@
 #include <QMainWindow>
 #include <vtkSmartPointer.h>
 #include <vtkRenderer.h>
+#include <vtkNamedColors.h>
+#include <QStatusBar>
+#include <vtkCubeSource.h>
+#include <vtkActor.h>
+#include <vtkProperty.h>
+#include <vtkCamera.h>
+#include <vtkPolyData.h>
+#include <vtkDataSetMapper.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkRenderer.h>
+#include <vtkNamedColors.h>
+#include <vtkNew.h>
+#include <vtkGenericOpenGLRenderWindow.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkSTLReader.h>
+#include <vtkPlane.h>
+#include <vtkSmartPointer.h>
+#include <vtkShrinkFilter.h>
+#include <vtkClipPolyData.h>
+#include <vtkPolyData.h>
+#include <vtkClipDataSet.h>
+#include <vtkAlgorithm.h>
+#include <QAbstractButton>
+
+
+
 
 namespace Ui {
     class MainWindow;
@@ -23,6 +50,12 @@ public slots:
     void handleSaveButton();
     void handleHelpButton();
     void handlePrintButton();
+    void handleModelColorButton();
+    void handleBackgroundColorButton();
+    void handleShrinkFilterCheckBox(bool ShrinkFilterStatus);
+    void handleClipFilterCheckBox(bool ClipFilterStatus);
+    void handleRenderCube();
+    void handleRenderSphere();
     
     // ---------------------------------------------------------------------
 
@@ -34,7 +67,20 @@ signals:
 private:
     Ui::MainWindow* ui;
 
-    vtkSmartPointer<vtkRenderer> renderer; //pointer for red
+    //Required Pointer Definitions
+    vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+    vtkSmartPointer<vtkActor> actor = vtkSmartPointer<vtkActor>::New();
+    vtkSmartPointer<vtkNamedColors> colors = vtkSmartPointer<vtkNamedColors>::New();
+    vtkSmartPointer<vtkDataSetMapper> mapper = vtkSmartPointer<vtkDataSetMapper>::New();
+    vtkSmartPointer<vtkSTLReader> reader = vtkSmartPointer<vtkSTLReader>::New();
+    vtkSmartPointer<vtkGenericOpenGLRenderWindow> renderWindow = vtkSmartPointer<vtkGenericOpenGLRenderWindow>::New();
+    vtkSmartPointer<vtkClipDataSet> ClipFilter = vtkSmartPointer<vtkClipDataSet>::New();
+    vtkSmartPointer<vtkShrinkFilter> ShrinkFilter = vtkSmartPointer<vtkShrinkFilter>::New();
+    vtkSmartPointer<vtkPlane> planeLeft = vtkSmartPointer<vtkPlane>::New();
+
+    vtkSmartPointer<vtkAlgorithm> source = vtkSmartPointer<vtkAlgorithm>::New();
+
+    bool ShrinkFilterStatus, ClipFilterStatus;
 
 };
 
